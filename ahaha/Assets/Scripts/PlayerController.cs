eed8bc579866c1c8756d8ef718731ab333e9bebf
hardday4 <83535337+hardday4@users.noreply.github.com>
2021-05-04 16:20:25 +0300

    game

--- ahaha/Assets/Scripts/PlayerController.cs
+++ ahaha/Assets/Scripts/PlayerController.cs
@@ -10,12 +10,15 @@ public class PlayerController : MonoBehaviour
     public LayerMask mask;
     public float radius;
     Rigidbody2D rb;
+    public float jumphigh = 8;
     public Animator anim;
     bool isGround;
     [SerializeField] Transform GroundCheck;
     int jumpValue=1;
+    SpriteRenderer sprite;  
     void Start()
     {
+        sprite = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
         joystick = FindObjectOfType<Joystick>();
         
@@ -23,17 +26,26 @@ public class PlayerController : MonoBehaviour
 
     
     void Update()
-    {
+    {   if (joystick.Horizontal > 0) { sprite.flipX = false; }
+        if (joystick.Horizontal < 0) { sprite.flipX = true; }
         anim.SetFloat("speed",Mathf.Abs(joystick.Horizontal));
         if (isGround == true) { jumpValue = 1; }
+        //StartCoroutine(enumerator());
     }
 
     private void FixedUpdate()
     {
         transform.Translate(new Vector3(joystick.Horizontal * speed * Time.deltaTime, 0, 0));
         isGround = Physics2D.OverlapCircle(GroundCheck.position, radius, mask);
+        
 
-        if (jumpValue>0) { rb.velocity = Vector2.up * joystick.Vertical*5; jumpValue--; }
+
+        if (jumpValue>0) 
+        { 
+            rb.velocity = Vector2.up * joystick.Vertical*jumphigh; 
+            jumpValue--;
+            StartCoroutine(enumerator());
+        }
         
 
 
@@ -41,4 +53,8 @@ public class PlayerController : MonoBehaviour
 
 
     }
+    IEnumerator enumerator()
+    { 
+        yield return new WaitForSecondsRealtime(5);
+    }   
 }

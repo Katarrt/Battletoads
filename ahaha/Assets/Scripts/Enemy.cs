3613856a12caa0b32a1dd42d0323676755283aa3
Shalapai <kstebenkov1@mail.ru>
2021-05-07 13:52:44 +0300

    a

--- ahaha/Assets/Scripts/Enemy.cs
+++ ahaha/Assets/Scripts/Enemy.cs
@@ -7,22 +7,33 @@ public class Enemy : MonoBehaviour
     Transform player;
     Vector2 target;
     public float speed;
+    SpriteRenderer sprite;
     void Start()
     {
+        sprite = GetComponent<SpriteRenderer>();
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         target = new Vector2(player.transform.position.x, player.transform.position.y);
 
     }
 
-    
+
     void Update()
     {
-        if (Vector2.Distance(transform.position,player.position)<7)
+        if (Vector2.Distance(transform.position, player.position) < 7)
         {
             transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
         }
-        
 
+        if (this.transform.position.x < player.transform.position.x)
+        {
+            sprite.flipX = true;
+
+        }
+        else
+        {
+            sprite.flipX = false;
+
+        }
 
 
     }
